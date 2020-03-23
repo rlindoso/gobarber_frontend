aa8b1393f0e6330899702fedb7a32efcab05e52a
Rafael Lindoso <rafael.lindoso@gmail.com>
2020-03-22 21:45:02 -0300

    Autenticação

--- src/store/modules/auth/reducer.js
+++ src/store/modules/auth/reducer.js
@@ -1,7 +1,18 @@
-const INICIAL_STATE = {};
+import produce from 'immer';
+
+const INICIAL_STATE = {
+  token: null,
+  signed: false,
+  loading: false,
+};
 
 export default function auth(state = INICIAL_STATE, action) {
   switch (action.type) {
+    case '@auth/SIGN_IN_SUCCESS':
+      return produce(state, draft => {
+        draft.token = action.payload.token;
+        draft.signed = true;
+      });
     default:
       return state;
   }
